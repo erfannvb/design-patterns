36239811870e08df3e1943aa20e73503c9ff9d4b
Erfan Navab <erfannavab@gmail.com>
2023-10-27 14:43:01 +0330

    test breaking singleton using reflection API

---
+++ src/main/java/nvb/dev/singleton/ReflectionSingleton.java
@@ -0,0 +1,28 @@
+package nvb.dev.singleton;
+
+import nvb.dev.singleton.lazy.LazySingleton;
+
+import java.lang.reflect.Constructor;
+
+public class ReflectionSingleton {
+    public static void main(String[] args) {
+
+        LazySingleton obj1 = LazySingleton.getInstance();
+        LazySingleton obj2 = null;
+
+        try {
+
+            Constructor<LazySingleton> constructor = LazySingleton.class.getDeclaredConstructor();
+            constructor.setAccessible(true);
+
+            obj2 = constructor.newInstance();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("Object 1 : " + obj1.hashCode());
+        System.out.println("Object 2 : " + obj2.hashCode());
+
+    }
+}
