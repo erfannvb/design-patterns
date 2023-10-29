c9afd199382df929b425873afc0f525aa79748a4
Erfan Navab <erfannavab@gmail.com>
2023-10-29 19:19:41 +0330

    add private constructor

--- src/main/java/nvb/dev/abstractFactory/FactoryProducer.java
+++ src/main/java/nvb/dev/abstractFactory/FactoryProducer.java
@@ -2,6 +2,9 @@ package nvb.dev.abstractFactory;
 
 public class FactoryProducer {
 
+    private FactoryProducer() {
+    }
+
     public static AbstractFactory getFactory(boolean isRounded) {
         if (isRounded) {
             return new RoundedShapeFactory();
